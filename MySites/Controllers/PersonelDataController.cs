b9c87a37cee5dbbc859407b83764899eda77debe
Osman Sivrikaya <osmansivrikaya927@gmail.com>
2022-11-23 11:26:34 +0300

    PersonalData Controller Ekleme Tasarım Düzenleme

--- MySites/Controllers/PersonelDataController.cs
+++ MySites/Controllers/PersonelDataController.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Core.Repository.Abstract;
+using Entities.Entity.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MySites.Controllers
 {
     public class PersonelDataController : Controller
     {
+        private readonly IPersonalDataRepository _personalDataRepository;
+        public PersonelDataController(IPersonalDataRepository personalDataRepository)
+        {
+            _personalDataRepository = personalDataRepository;
+        }
         [Authorize(Roles = "Admin")]
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> PersonalDataGuncelle(PersonalData personalData)
+        {
+            personalData.Id = 1;
+            await _personalDataRepository.UpdateAsync(personalData);
+            return RedirectToAction("Index","PersonelData");
         }
     }
 }
